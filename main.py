5a8e9dfca3e0105a0fe841d4a16b0270b319c080
Dioni Alves <dionialves@gmail.com>
2023-10-09 08:14:31 -0300

    implemented automatic resource update functionality up to a certain level

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from village import Village
 from selenium.webdriver.common.by import By
 import time
 
-server = "ts3.x1.america.travian.com"
-username = "diviks"
-password = "alves625"
+#Coloque aqui as informações de conexão com o servidor
+server = ""
+username = ""
+password = ""
 
 
 village = Village()
@@ -52,18 +53,36 @@ while True:
         for x in range(18):
             print("iniciando laço de numero " + str(x+1))
             
-            print("verificando se campo é menor que 4")
-            if int(village.fields['Aldeia do diviks']['level'][x]) < 4:
+            print("verificando se campo é menor que 5")
+            if int(village.fields['Aldeia do diviks']['level'][x]) < 5:
                 print("Construindo campo")
-                village.upgrade_fields_resource('Aldeia do diviks', x+1)
-                break
+
+                # Atualiza os recursos da aldeia
+                village.get_resources('Aldeia do diviks')
+
+                # retorna lista com recursos necessários para fazer a construção
+                resources = village.check_construction_resources(x+1)
+
+                if (int(village.resources['Aldeia do diviks']['lumber']) >= int(resources['lumber']) and
+                    int(village.resources['Aldeia do diviks']['clay']) >= int(resources['clay'])  and
+                    int(village.resources['Aldeia do diviks']['iron']) >= int(resources['iron'])  and
+                    int(village.resources['Aldeia do diviks']['crop']) >= int(resources['crop'])):
+
+                    village.upgrade_fields_resource('Aldeia do diviks', x+1)
+                    village.update_building_orders('Aldeia do diviks')
+                    break
 
             else:
                 print("Campo já esta no nível desejado")
 
     else:
         print("Identificado que já tem construções na fila")
-            
-    print("iniciando espera de 20minutos")
-    time.sleep(1200)
+
+    if village.building_ordens['Aldeia do diviks']:
+        print(f'Tempo de espera: {round(int(village.building_ordens["Aldeia do diviks"][0][2]) / 60 + 2, 0)} minutos')
+        time.sleep(int(village.building_ordens['Aldeia do diviks'][0][2] + 120))
+    
+    else:
+        print("Sem recursos suficientes para construir, vamos aguardar 20 minutos")
+        time.sleep(600)
 
